916e666320c00bf0ad8e763e25fde827784282d3
JD Willemse <fuzzyvagina@gmail.com>
2023-12-14 16:14:21 +0100

    output test file for printing

--- index.mjs
+++ index.mjs
@@ -1,3 +1,17 @@
+import fs from "fs";
 import colors from "colors/safe.js";
 
 console.log("\n", colors.rainbow("––––– TEST SUCCESS! –––––"), "\n\n");
+
+const OUTPUT_FILE = "print-data.txt";
+
+const currentTime = new Date().toLocaleString("en-US", {
+  timeZone: "America/New_York",
+});
+
+let data = "The current time is\n" + currentTime;
+
+fs.writeFileSync(OUTPUT_FILE, data);
+console.log("File written successfully");
+console.log("------------------------");
+console.log(fs.readFileSync(OUTPUT_FILE, "utf8"));
